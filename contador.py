3ab1385996f33bad488e96d5ad9dfc8b4f6c1034
joag <joaggi@gmail.com>
2022-03-14 09:21:51 -0500

    Renombre y agrecación de función a contador.py

--- contador.py
+++ contador.py
@@ -2,6 +2,9 @@
 
 from collections import Counter
 
+def contar_letras(x: str) -> Counter:
+    return Counter(x)
+
 if __name__ == "__main__":
     frase = "Hola Mundo, estamos en el curso de metodologías agiels y machine learning"
-    print(Counter(frase))
+    print(contar_letras(frase))
